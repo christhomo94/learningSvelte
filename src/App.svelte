3cc636344b62ad21436198965747f23a844a5dcf
chrisThomason <chris.thomason1994@gmail.com>
2021-07-25 16:00:55 +0100

    First Commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	let name = "Chris";
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>This is your first Svelte App! </p>
+</main>
+
+<style>
+	
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 800;
+	}
+
+	p {
+		font-size: 1.5em;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
